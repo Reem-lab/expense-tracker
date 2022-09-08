3b81a2ee8409afc374f28f86a34dfe3b55370cb1
reem <janinareem@gmail.com>
2022-09-09 00:49:45 +0200

    Add methods to pass it from the childs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 
 function App() {
   const expenses = [
@@ -24,28 +26,14 @@ function App() {
     },
   ];
 
+  const addExpense = ( newExpenseData ) => {
+    console.log('hello form app.js')
+  }
+
   return (
     <div className="App">
-  <ExpenseItem 
-  title={expenses[0].title}
-  amount={[expenses[0].amount]}
-  date={expenses[0].date}
-  />
-  <ExpenseItem 
-   title={expenses[1].title}
-   amount={[expenses[1].amount]}
-   date={expenses[1].date}
-  />
-  <ExpenseItem 
-   title={expenses[2].title}
-   amount={[expenses[2].amount]}
-   date={expenses[2].date}
-  />
-  <ExpenseItem 
-   title={expenses[3].title}
-   amount={[expenses[3].amount]}
-   date={expenses[3].date}
-  />
+      <NewExpense onAddExpense={addExpense} />
+       <Expenses items={expenses} />
     </div>
   );
 }
