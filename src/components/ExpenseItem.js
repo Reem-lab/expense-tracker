62d22a2c2234999a40115f4723a4d878ca2e35f4
reem <janinareem@gmail.com>
2022-09-10 03:50:48 +0200

    Update Expenseitem file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,14 @@
-import { useState } from 'react';
 import ExpenseData from './ExpenseData';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
- const [title, setTtile] = useState(props.title);
-
-const titleHandler = () => {
-     setTtile('Updated!');
-}
 
   return (
     <div className='expense-item'>
       <ExpenseData date={props.date} />
       <div className='expense-item__description'>
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className='expense-item__price'>{props.amount}</div>
       </div>
-      <button onClick={titleHandler}>Change title</button>
     </div>
   )
 }
