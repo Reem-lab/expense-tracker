8efb8e9afcaccde59ac0929ef11adc8673636ed4
reem <janinareem@gmail.com>
2022-09-10 03:53:14 +0200

    update the app file to make it statfull

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const Dummy_expenses = [
+  {
+    id: 'e1',
+    title: "car insurance",
+    amount: 450,
+    date:  new Date(2021, 8, 20),
+  },
+  {
+    id: 'e2',
+    title: "grocercy needs",
+    amount: 8020,
+    date:  new Date(2022, 5, 3),
+  },
+  {
+    id: 'e3',
+    title: "goods",
+    amount: 960,
+    date:  new Date(2022, 8, 25),
+  },
+  {
+    id: 'e4',
+    title: "cloths",
+    amount: 2500,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      date:  new Date(2021, 8, 20),
-      title: "car insurance",
-      amount: 450,
-    },
-    {
-      date:  new Date(2022, 5, 3),
-      title: "grocercy needs",
-      amount: 8020,
-    },
-    {
-      date:  new Date(2022, 8, 25),
-      title: "goods",
-      amount: 960,
-    },
-    {
-      date: new Date(2021, 5, 12),
-      title: "cloths",
-      amount: 2500,
-    },
-  ];
+  const [expenses, setExpenses] = useState(Dummy_expenses);
 
   const addExpense = ( newExpenseData ) => {
-    console.log('hello form app.js')
+    setExpenses((prevExpenses) => {
+      return [newExpenseData, ...prevExpenses ];
+    })
   }
 
   return (
