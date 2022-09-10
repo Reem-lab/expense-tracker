62528966e1ae1ac8295b0cfef27bc599131ed546
reem <janinareem@gmail.com>
2022-09-10 03:51:51 +0200

    add dyncamic list for expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -10,29 +11,26 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem 
-  title={props.items[0].title}
-  amount={[props.items[0].amount]}
-  date={props.items[0].date}
-  />
-  <ExpenseItem 
-   title={props.items[1].title}
-   amount={[props.items[1].amount]}
-   date={props.items[1].date}
-  />
-  <ExpenseItem 
-   title={props.items[2].title}
-   amount={[props.items[2].amount]}
-   date={props.items[2].date}
-  />
-  <ExpenseItem 
-   title={props.items[3].title}
-   amount={[props.items[3].amount]}
-   date={props.items[3].date}
-  />
+      <ExpensesFilter 
+      selected={filteredYear} 
+      onChangeFilter={filterChangeHandler} />
+<ExpensesChart expenses={filteredExpenses} />
+{filteredExpenses.length === 0 && <p className='expenses-list__fallback'>No Expenses Found!</p>}
+{filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+  ))
+}
     </div>
   )
 }
